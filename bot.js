7d47da90f91d09a198cd7e7d27f5a5f3cb86b234
maxrchung <maxrchung@gmail.com>
2016-04-22 15:41:31 -0400

    Simple bot that responds to !cry and !count; uses low and facebook chat api

---
+++ bot.js
@@ -0,0 +1,56 @@
+var credentials = require('./credentials')
+var login = require('facebook-chat-api')
+var low = require('lowdb')
+var storage = require('lowdb/file-sync')
+var db = low('counts.json', { storage })
+
+login(credentials.data, handleLogin)
+
+function handleLogin(err, api) {
+    if (err) return console.error(err)
+
+    api.setOptions({
+	logLevel: "silent"
+    })
+
+    handleAPI(api)
+}
+
+function handleAPI(api) {
+    var cryFace = '\u{1F602}'
+    api.listen(function processMessages(err, message) {
+	if (err) return console.error(err)
+
+	var currentCountObj = db('counts').find({ threadID: message.threadID })
+	var currentCount
+	if (currentCountObj !== undefined) {
+	    currentCount = currentCountObj['count']
+	}
+	else {
+	    currentCount = 0
+	    db('counts').push({ threadID: message.threadID, count: 0 })
+	}
+
+	var counter = 0
+	if (message.body === '!cry') {
+	    api.sendMessage(cryFace, message.threadID)
+	    counter = 1
+	}
+	else if (message.body === '!count') {
+	    var msg = { body: currentCount }
+	    api.sendMessage(msg, message.threadID)
+	}
+	else {
+	    counter = message.body.split(cryFace).length - 1
+	}
+
+	if (counter > 0) {
+	    var update = currentCount + counter
+	    db('counts')
+		.chain()
+		.find({ threadID: message.threadID })
+		.assign({ count: update })
+		.value()
+	}
+    })
+}
