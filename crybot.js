8ed7063ea69e08a0b3d0855eeb6d6e76d7c0d64f
maxrchung <maxrchung@gmail.com>
2016-07-11 22:22:07 -0400

    Automatic restart attempt #1 pls work

--- crybot.js
+++ crybot.js
@@ -1,19 +1,32 @@
 var credentials = require('./credentials')
 var login = require('facebook-chat-api')
 var low = require('lowdb')
-var storage = require('lowdb/file-sync')
+var storage = require('lowdb/lib/file-sync')
 var db = low('counts.json', { storage })
 var fs = require('fs')
 
-login(credentials.data, handleLogin)
+// Literally bad
+state = 'fine'
+/*
+state = 'ohgod'
+state = 'ohgod2'
+*/
+stateData = 0
+
+function run() {
+    login(credentials.data, handleLogin)
+}
+
+run()
 
 function handleLogin(err, api) {
     if (err) {
 	fs.appendFileSync('log.txt', err)
+	fs.appendFileSync('log.txt', '\n')
     }
 
     api.setOptions({
-	logLevel: "silent"
+	logLevel: 'silent'
     })
 
     handleAPI(api)
@@ -24,26 +37,55 @@ function handleAPI(api) {
     api.listen(function processMessages(err, message) {
 	if (err) {
 	    fs.appendFileSync('log.txt', err)
+	    fs.appendFileSync('log.txt', '\n')
 	}
 
-	var currentCountObj = db('counts').find({ threadID: message.threadID })
-	var currentCount
-	if (currentCountObj !== undefined) {
-	    currentCount = currentCountObj['count']
-	}
-	else {
-	    currentCount = 0
-	    db('counts').push({ threadID: message.threadID, count: 0 })
+	if (state == 'ohgod') {
+	    api.sendMessage(cryFace, message.threadID, function callback(err, message) {})
+	    state = 'fine'
+	} 
+	else if (state == 'ohgod2') {
+	    var msg = { body: String(stateData) }
+	    api.sendMessage(msg, message.threadID, function callback(err, message) {})
+	    state = 'fine'
 	}
 
+	var currentCountObj = db.get('counts').find({ threadID: message.threadID }).value()
+	var currentCount = currentCountObj['count']
 	var counter = 0
+
 	if (message.body === '!cry') {
-	    api.sendMessage(cryFace, message.threadID)
+	    api.sendMessage(cryFace, message.threadID, function callback(err, message) {
+		if (err) {
+		    fs.appendFileSync('log.txt', '!cry error\n')
+		    fs.appendFileSync('log.txt', err.error)
+		    fs.appendFileSync('log.txt', '\n')
+		    fs.appendFileSync('log.txt', err.detail)
+		    fs.appendFileSync('log.txt', '\n')
+		    state = 'ohgod'
+		    handleAPI(api)
+		    return stopListening()
+		}
+	    })
 	    counter = 1
 	}
 	else if (message.body === '!count') {
-	    var msg = { body: currentCount }
-	    api.sendMessage(msg, message.threadID)
+	    var msg = { body: String(currentCount) }
+	    api.sendMessage(msg, message.threadID, function callback(err, message) {
+		if (err) {
+		    fs.appendFileSync('log.txt', '!count error\n')
+		    fs.appendFileSync('log.txt', err)
+		    fs.appendFileSync('log.txt', '\n')
+		    fs.appendFileSync('log.txt', err.error)
+		    fs.appendFileSync('log.txt', '\n')
+		    fs.appendFileSync('log.txt', err.detail)
+		    fs.appendFileSync('log.txt', '\n')
+		    state = 'ohgod2'
+		    stateData = currentCount
+		    handleAPI(api)
+		    return stopListening()
+		}
+	    })
 	}
 	else if (message.body) {
 	    counter = message.body.split(cryFace).length - 1
@@ -51,7 +93,7 @@ function handleAPI(api) {
 
 	if (counter > 0) {
 	    var update = currentCount + counter
-	    db('counts')
+	    db.get('counts')
 		.chain()
 		.find({ threadID: message.threadID })
 		.assign({ count: update })
