b0e44cdfc3bdf03c90ecc2f5466a69d931b139ae
maxrchung <maxrchung@gmail.com>
2016-07-24 22:01:41 -0400

    Another attempt at restarting

--- crybot.js
+++ crybot.js
@@ -5,13 +5,8 @@ var storage = require('lowdb/lib/file-sync')
 var db = low('counts.json', { storage })
 var fs = require('fs')
 
-// Literally bad
-state = 'fine'
-/*
-state = 'ohgod'
-state = 'ohgod2'
-*/
-stateData = 0
+var checkTime = Date.now()
+var rebootTime = 3600000
 
 function run() {
     login(credentials.data, handleLogin)
@@ -34,78 +29,77 @@ function handleLogin(err, api) {
 
 function handleAPI(api) {
     var cryFace = '\u{1F602}'
-    api.listen(function processMessages(err, message) {
+    var stopListening = api.listen(function processMessages(err, message) {
 	if (err) {
 	    fs.appendFileSync('log.txt', err)
 	    fs.appendFileSync('log.txt', '\n')
 	}
 
-	if (state == 'ohgod') {
-	    api.sendMessage(cryFace, message.threadID, function callback(err, message) {})
-	    state = 'fine'
-	} 
-	else if (state == 'ohgod2') {
-	    var msg = { body: String(stateData) }
-	    api.sendMessage(msg, message.threadID, function callback(err, message) {})
-	    state = 'fine'
+	if (message) {
+	    var currentCountObj = db.get('counts').find({ threadID: message.threadID }).value()
+	    var currentCount = currentCountObj['count']
+	    var counter = 0
+
+	    if (message.body === '!cry') {
+		api.sendMessage(cryFace, message.threadID, function callback(err, message) {		    
+		    if (err) {
+			fs.appendFileSync('log.txt', '!cry error\n')
+			fs.appendFileSync('log.txt', err.error)
+			fs.appendFileSync('log.txt', '\n')
+			fs.appendFileSync('log.txt', err.detail)
+			fs.appendFileSync('log.txt', '\n')
+		    }
+
+		    if (Date.now() - checkTime > rebootTime) {
+			checkTime = Date.now()
+			login(credentials.data, function(err, api) {
+			    handleLogin(err, api)
+			    return stopListening()
+			})
+		    }
+		})
+		counter = 1
+	    }
+	    else if (message.body === '!count') {
+		var msg = { body: String(currentCount) }
+		api.sendMessage(msg, message.threadID, function callback(err, message) {
+		    if (err) {
+			fs.appendFileSync('log.txt', '!count error\n')
+			fs.appendFileSync('log.txt', err)
+			fs.appendFileSync('log.txt', '\n')
+			fs.appendFileSync('log.txt', err.error)
+			fs.appendFileSync('log.txt', '\n')
+			fs.appendFileSync('log.txt', err.detail)
+			fs.appendFileSync('log.txt', '\n')
+		    }
+
+		    if (Date.now() - checkTime > rebootTime) {
+			checkTime = Date.now()
+			login(credentials.data, function(err, api) {
+			    handleLogin(err, api)
+			    return stopListening()
+			})
+		    }
+		})
+	    }
+	    else if (message.body) {
+		counter = message.body.split(cryFace).length - 1
+	    }
+
+	    if (counter > 0) {
+		var update = currentCount + counter
+		db.get('counts')
+		    .chain()
+		    .find({ threadID: message.threadID })
+		    .assign({ count: update })
+		    .value()
+	    }
 	}
 
-	var currentCountObj = db.get('counts').find({ threadID: message.threadID }).value()
-	var currentCount = currentCountObj['count']
-	var counter = 0
-
-	if (message.body === '!cry') {
-	    api.sendMessage(cryFace, message.threadID, function callback(err, message) {
-		if (err) {
-		    fs.appendFileSync('log.txt', '!cry error\n')
-		    fs.appendFileSync('log.txt', err.error)
-		    fs.appendFileSync('log.txt', '\n')
-		    fs.appendFileSync('log.txt', err.detail)
-		    fs.appendFileSync('log.txt', '\n')
-		    fs.appendFileSync('log.txt', 'Rerunning bot')
-		    fs.appendFileSync('log.txt', '\n')
-		    state = 'ohgod'
-		    run()
-		    fs.appendFileSync('log.txt', 'After rerunning')
-		    fs.appendFileSync('log.txt', '\n')
-		    return stopListening()
-		}
-	    })
-	    counter = 1
-	}
-	else if (message.body === '!count') {
-	    var msg = { body: String(currentCount) }
-	    api.sendMessage(msg, message.threadID, function callback(err, message) {
-		if (err) {
-		    fs.appendFileSync('log.txt', '!count error\n')
-		    fs.appendFileSync('log.txt', err)
-		    fs.appendFileSync('log.txt', '\n')
-		    fs.appendFileSync('log.txt', err.error)
-		    fs.appendFileSync('log.txt', '\n')
-		    fs.appendFileSync('log.txt', err.detail)
-		    fs.appendFileSync('log.txt', '\n')
-		    stateData = currentCount
-		    fs.appendFileSync('log.txt', 'Rerunning bot')
-		    fs.appendFileSync('log.txt', '\n')
-		    state = 'ohgod2'
-		    run()
-		    fs.appendFileSync('log.txt', 'After rerunning')
-		    fs.appendFileSync('log.txt', '\n')
-		    return stopListening()
-		}
-	    })
-	}
-	else if (message.body) {
-	    counter = message.body.split(cryFace).length - 1
-	}
-
-	if (counter > 0) {
-	    var update = currentCount + counter
-	    db.get('counts')
-		.chain()
-		.find({ threadID: message.threadID })
-		.assign({ count: update })
-		.value()
+	if (Date.now() - checkTime > rebootTime) {
+	    checkTime = Date.now()
+	    login(credentials.data, handleLogin)
+	    return stopListening()
 	}
     })
 }
